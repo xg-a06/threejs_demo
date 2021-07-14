f5f0663e952e553446ec70980af492b912e35409
xg-a06 <xg_jser@163.com>
2021-07-14 08:30:52 +0800

    提交demo04

--- demo/demo03/index.ts
+++ demo/demo03/index.ts
@@ -1,30 +1,82 @@
 import {
   Scene,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
+  AxesHelper,
   PerspectiveCamera,
   WebGLRenderer,
+  Vector3,
+  BufferGeometry,
+  LineBasicMaterial,
+  Color,
+  Float32BufferAttribute,
+  Line,
 } from 'three';
 
-const scene = new Scene();
+import { WEBGL } from 'three/examples/jsm/WebGL';
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0xff0000 });
-const cube = new Mesh(geometry, material);
+let scene: Scene;
+let geometry: BufferGeometry;
+let material: LineBasicMaterial;
+let camera: PerspectiveCamera;
+let renderer: WebGLRenderer;
 
-scene.add(cube);
+function initScene() {
+  scene = new Scene();
+}
 
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+function initLight() {
+  console.log('init light');
+}
 
-const renderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function initObject() {
+  geometry = new BufferGeometry();
+  material = new LineBasicMaterial({ vertexColors: true });
+  const positions = new Float32Array([-10, 0, 10, 10, 0, -10]);
+  const color1 = new Color(0x00ff00);
+  const color2 = new Color(0xff0000);
+  const colors = new Float32Array([color1.r, color1.g, color1.b, color2.r, color2.g, color2.b]);
+  geometry.setAttribute('position', new Float32BufferAttribute(positions, 3));
+  geometry.setAttribute('color', new Float32BufferAttribute(colors, 3));
+  const line = new Line(geometry, material);
+  const axes = new AxesHelper(10);
+  scene.add(line);
+  scene.add(axes);
+}
+
+function initCamera() {
+  camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.x = 100;
+  camera.position.y = 100;
+  camera.position.z = 100;
+  camera.up.x = 0;
+  camera.up.y = 1;
+  camera.up.z = 0;
+  camera.lookAt(new Vector3(0, 0, 0));
+}
+
+function initRenderer() {
+  renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
 
 function render() {
   requestAnimationFrame(render);
-  cube.rotation.x += 0.1;
   renderer.render(scene, camera);
 }
-render();
+
+function work() {
+  if (WEBGL.isWebGLAvailable()) {
+    initScene();
+    initLight();
+    initObject();
+    initCamera();
+    initRenderer();
+    render();
+  } else {
+    console.log(WEBGL.getWebGLErrorMessage());
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  work();
+});
--- demo/demo04/index.ts
+++ demo/demo04/index.ts
@@ -1,30 +1,85 @@
 import {
   Scene,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
+  AxesHelper,
   PerspectiveCamera,
   WebGLRenderer,
+  Vector3,
+  BufferGeometry,
+  LineBasicMaterial,
+  Color,
+  Float32BufferAttribute,
+  Line,
 } from 'three';
 
-const scene = new Scene();
+import { WEBGL } from 'three/examples/jsm/WebGL';
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0xff0000 });
-const cube = new Mesh(geometry, material);
+let scene: Scene;
+let camera: PerspectiveCamera;
+let renderer: WebGLRenderer;
 
-scene.add(cube);
+function initScene() {
+  scene = new Scene();
+}
 
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+function initLight() {
+  console.log('init light');
+}
 
-const renderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function initObject() {
+  const geometry = new BufferGeometry();
+  geometry.setFromPoints([new Vector3(-100, 0, 0), new Vector3(100, 0, 0)]);
+  const materialX = new LineBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.8 });
+  const materialY = new LineBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.8 });
+  for (let i = 0; i < 11; i += 1) {
+    const lineX = new Line(geometry, materialX);
+    lineX.position.y = 100 - i * 20;
+    scene.add(lineX);
+
+    const lineY = new Line(geometry, materialY);
+    lineY.position.x = i * 20 - 100;
+    lineY.rotation.z = (90 * Math.PI) / 180;
+    scene.add(lineY);
+  }
+
+  const axes = new AxesHelper(500);
+  scene.add(axes);
+}
+
+function initCamera() {
+  camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 2000;
+  camera.up.x = 0;
+  camera.up.y = 1;
+  camera.up.z = 0;
+  camera.lookAt(new Vector3(0, 0, 0));
+}
+
+function initRenderer() {
+  renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
 
 function render() {
   requestAnimationFrame(render);
-  cube.rotation.x += 0.1;
   renderer.render(scene, camera);
 }
-render();
+
+function work() {
+  if (WEBGL.isWebGLAvailable()) {
+    initScene();
+    initLight();
+    initObject();
+    initCamera();
+    initRenderer();
+    render();
+  } else {
+    console.log(WEBGL.getWebGLErrorMessage());
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  work();
+});
